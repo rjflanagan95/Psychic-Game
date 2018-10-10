9cd5e68adbc18902dab4c99c23b15566b9454ee1
Robert Flanagan <robertflanagan95@gmail.com>
2018-10-09 20:42:17 -0400

    fixed text issue, but now the game doesn't recognize wins

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,42 @@
+// the computer will pick a random letter
 var computerOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
 console.log("Computer choice: " + computerChoice);
 
+// setting up variables
 var numWins = 0;
 var numLosses = 0;
 var attempts = 9;
+
+// the user's guess
 var letter = "";
+// this array will store the letters already guessed
 var guesses = [];
+
+// assigning variables to the HTML elements we're changing
 var winsText = document.getElementById("wins");
 var lossesText = document.getElementById("losses");
 var attemptsText = document.getElementById("guesses-left");
 var guessesText = document.getElementById("already-guessed");
 
+// when a key is pressed, the letter is stored in the "letter" variable
 document.onkeyup = function(event) {
     var letter = event.key.toLowerCase();
     console.log("User input: " + letter);
 
     
-    if (letter === computerChoice) {
+    if (letter == computerChoice) {
         numWins += 1;
-        console.log("numWins: " + numWins);
-        attempts = 9;
         winsText.textContent = numWins;
-        attemptsText.textContent = attemptsText;
+        // console.log("numWins: " + numWins);
+        attempts = 9;
+        attemptsText.textContent = attempts;
+        guesses = [];
+        guessesText.textContent = guesses;
         
     } else {
         attempts -= 1;
-        console.log("Guesses left: " + attempts);
+        // console.log("Guesses left: " + attempts);
         attemptsText.textContent = attempts;
         guesses += letter;
         guessesText.textContent = guesses;
@@ -34,11 +44,14 @@ document.onkeyup = function(event) {
     }
     if (attempts === 0) {
         numLosses += 1;
-        console.log("numLosses: " + numLosses);
-        attempts = 9;
         lossesText.textContent = numLosses;
+        // console.log("numLosses: " + numLosses);
+        attempts = 9;
         attemptsText.textContent = attempts;
         guesses = [];
         guessesText.textContent = guesses;
+        // restarting the game
+        var computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+        console.log("Computer choice: " + computerChoice);
     }
 }
