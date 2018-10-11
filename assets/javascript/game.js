0f115fc35e8949ee74e5b576cb0db30af579c1dc
Robert Flanagan <robertflanagan95@gmail.com>
2018-10-11 13:00:24 -0400

    added conditions for game start, comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,7 @@
 var computerOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
 
+// for verifying functionality
 console.log("Computer choice: " + computerChoice);
 
 // setting up variables
@@ -9,10 +10,18 @@ var numWins = 0;
 var numLosses = 0;
 var attempts = 9;
 
-// the user's guess
-var letter = "";
 // this array will store the letters already guessed
 var guesses = [];
+
+// this function will reset the game after a win or loss
+function resetGame() {
+    attempts = 9;
+    attemptsText.textContent = attempts;
+    guesses = [];
+    guessesText.textContent = guesses;
+    computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+    console.log(computerChoice);
+}
 
 // assigning variables to the HTML elements we're changing
 var winsText = document.getElementById("wins");
@@ -24,36 +33,26 @@ var guessesText = document.getElementById("already-guessed");
 document.onkeyup = function(event) {
     var letter = event.key.toLowerCase();
 
-    // conditions for a win, else lose an attempt
-    if (letter == computerChoice) {
-        numWins += 1;
-        winsText.textContent = numWins;
+    // can only play using letters of the alphabet, so this will check that the input exists in the array computerOptions
+    if (computerOptions.indexOf(letter) > -1) {
+        // conditions for a win, else lose an attempt
+        if (letter == computerChoice) {
+            numWins += 1;
+            winsText.textContent = numWins;
+            resetGame();
 
-        // restarting the game after a win
-        attempts = 9;
-        attemptsText.textContent = attempts;
-        guesses = [];
-        guessesText.textContent = guesses;
-        computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
-        console.log(computerChoice);
-    } else {
-        attempts -= 1;
-        attemptsText.textContent = attempts;
-        guesses += letter;
-        guessesText.textContent = guesses;
-        
-    }
-    // conditions for a loss
-    if (attempts === 0) {
-        numLosses += 1;
-        lossesText.textContent = numLosses;
+        } else {
+            attempts -= 1;
+            attemptsText.textContent = attempts;
+            guesses += letter;
+            guessesText.textContent = guesses;
+        }
 
-        // restarting the game after a loss
-        attempts = 9;
-        attemptsText.textContent = attempts;
-        guesses = [];
-        guessesText.textContent = guesses;
-        computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
-        console.log(computerChoice);
+        // conditions for a loss
+        if (attempts === 0) {
+            numLosses += 1;
+            lossesText.textContent = numLosses;
+            resetGame();
+        }
     }
 }
